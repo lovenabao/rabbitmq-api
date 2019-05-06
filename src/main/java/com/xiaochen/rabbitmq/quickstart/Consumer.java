5b9f718e5107be6d4b2057230800009f2e66f53a
lovenabao <486386254@qq.com>
2019-05-06 15:20:03 +0800

    Initial commit

---
+++ src/main/java/com/xiaochen/rabbitmq/quickstart/Consumer.java
@@ -0,0 +1,31 @@
+package com.xiaochen.rabbitmq.quickstart;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author xiaochen
+ * @create 2019-05-06 10:08
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws Exception{
+        //1.创建一个ConnectionFactory,并进行配置
+        ConnectionFactory connectionFactory =new ConnectionFactory();
+        connectionFactory.setHost("120.78.4.81");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        //2，通过连接工厂，创建一个真实的连接
+        Connection connection = connectionFactory.newConnection();
+        //3.通过connection，创建一个Channel
+        Channel channel = connection.createChannel();
+        //4.通过channel发送数据
+        for (int i = 0; i < 5; i++) {
+            String meg="Hello RabbitMQ!";
+            channel.basicPublish("","test001",null,meg.getBytes());
+        }
+
+    }
+
+}
---
+++ src/main/java/com/xiaochen/rabbitmq/quickstart/Procuder.java
@@ -0,0 +1,10 @@
+package com.xiaochen.rabbitmq.quickstart;
+
+/**
+ * @author xiaochen
+ * @create 2019-05-06 10:07
+ */
+public class Procuder {
+
+
+}
