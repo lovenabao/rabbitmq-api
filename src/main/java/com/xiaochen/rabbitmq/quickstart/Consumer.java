41f902f28d7f101edef2b78f5ad7bba3656c21ad
lovenabao <486386254@qq.com>
2019-05-06 17:14:59 +0800

    111

--- src/main/java/com/xiaochen/rabbitmq/quickstart/Consumer.java
+++ src/main/java/com/xiaochen/rabbitmq/quickstart/Consumer.java
@@ -1,8 +1,6 @@
 package com.xiaochen.rabbitmq.quickstart;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 /**
  * @author xiaochen
@@ -20,10 +18,19 @@ public class Consumer {
         Connection connection = connectionFactory.newConnection();
         //3.通过connection，创建一个Channel
         Channel channel = connection.createChannel();
-        //4.通过channel发送数据
-        for (int i = 0; i < 5; i++) {
-            String meg="Hello RabbitMQ!";
-            channel.basicPublish("","test001",null,meg.getBytes());
+        //4.声明（创建）一个队列
+        channel.queueDeclare("test001",true,false,false,null);
+        //5.创建消费者
+        QueueingConsumer queueingConsumer =new QueueingConsumer(channel);
+        //6.设置 channel
+        channel.basicConsume("test001",true,queueingConsumer);
+        while (true){
+            //7.获取消息
+            QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
+            String str=new String(delivery.getBody());
+            System.out.println("消费端： "+str);
+            //Envelope envelope = delivery.getEnvelope();
+
         }
 
     }
